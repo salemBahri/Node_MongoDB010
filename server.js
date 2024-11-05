e4c9cc76e0b5ac3fbdcae3c02cdc6c75eb9b8788
Salem Bahri <bahrisalem5@gmail.com>
2024-11-05 01:41:10 +0100

    add data to mongodb(json)

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express=require('express');
+const User=require('./models/user');
+
 require('./config/connect');
 
-
 const app=express();
+//accept data from frontend or postman(de typejson)
+app.use(express.json());
 
-app.post('/add',()=>{
 
-    console.log('add work');
-    
-})
+app.post('/add',(req,res)=>{
+
+    data=req.body;
+    //create instance for insert in database
+    usr=new User(data);
+
+    //for save in DB*(mongoDB)
+    usr.save()
+    //this for controle request effected or not
+        .then(
+            (savedUser)=>{
+                res.send(savedUser)
+            }
+        )
+        .catch(
+            (err)=>{
+                res.send(err)
+            }
+        )
+
+
+   
+});
 app.get('/getall',()=>{
     console.log('get work');
     
 })
-
-
-
 
 
 
