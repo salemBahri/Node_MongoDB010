65233f0aef52407ea36a8121e8c1322025bfc614
Salem Bahri <bahrisalem5@gmail.com>
2024-11-05 01:05:51 +0100

    link project with mongodb

---
+++ config/connect.js
@@ -0,0 +1,20 @@
+//import mongoose
+const mongoose=require('mongoose')
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
+.then(
+    ()=>{
+        console.log('connected');
+        
+    }
+)
+.catch(
+    (err)=>{
+        console.log(err);
+        
+    }
+)
+
+
+module.exports=mongoose;
