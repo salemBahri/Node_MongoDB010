52e3eb685b8db72cdbf3ed405a456253fd89bd78
Salem Bahri <bahrisalem5@gmail.com>
2024-11-06 16:15:24 +0100

    endpoint for get image

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use('/product',productRoute)
 app.use('/user',userRoute)
 
 
+//endpoint for get image 
+//http://127.0.0.1:3000/getimage/1730905883645.jpeg  ==>link for display image
+app.use('/getimage',express.static('./uploads'))
+
 
 app.listen(3000,()=>{
     console.log('server work');
